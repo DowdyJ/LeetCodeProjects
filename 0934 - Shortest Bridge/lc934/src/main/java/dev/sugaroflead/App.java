4db70272ef7ca921039ba0d8d16fca577b3de0ab
DowdyJ <joeldowdy12@gmail.com>
2023-05-23 08:11:15 +0900

    Finish 0934

--- 0934 - Shortest Bridge/lc934/src/main/java/dev/sugaroflead/App.java
+++ 0934 - Shortest Bridge/lc934/src/main/java/dev/sugaroflead/App.java
@@ -15,106 +15,55 @@ public class App
     }
 
     public int shortestBridge(int[][] grid) {
-        int answer = 0;
-        int[] islandTwoBlock = new int[] {};
-        boolean islandOneFound = false;
-        boolean islandTwoFound = false;
-        for (int i = 0; i < grid.length; ++i) {
-            for (int j = 0; j < grid[0].length; ++j) {
-                if (!islandOneFound && grid[i][j] == 1) {
-                    islandOneFound = true;
-                    DFS(grid, i, j);
-                }
-
-                if (islandOneFound && grid[i][j] == 1) {
-                    islandTwoBlock = new int[] {i, j};
-                    islandTwoFound = true;
+        Queue<int[]> queue = new LinkedList<>();
+        boolean[][] visited = new boolean[grid.length][grid[0].length];
+        boolean found = false;
+        for(int i=0;i<grid.length;i++){
+            if(found) break;
+            for(int j=0;j<grid[0].length;j++){
+                if(grid[i][j] == 1){
+                    DFS(grid, i, j, queue, visited);
+                    found = true;
                     break;
                 }
             }
-            if (islandTwoFound)
-                break;
         }
-
-        answer = BFS(grid, islandTwoBlock[0], islandTwoBlock[1]);
-
-        return answer;
+        return BFS(queue, visited, grid);
     }
-
-    private void DFS(int[][] grid, int i, int j) {
-        if (i > grid.length - 1 || i < 0 || j > grid[0].length - 1 || j < 0 || grid[i][j] == 0 || grid[i][j] == 2)
+    
+    private void DFS(int[][] grid, int i, int j, Queue<int[]> queue, boolean[][] visited){
+        if(i<0 || j<0 || i>=grid.length || j>=grid[0].length || visited[i][j] || grid[i][j] == 0) 
             return;
-
-        grid[i][j] = 2;
-
-        DFS(grid, i + 1, j);
-        DFS(grid, i - 1, j);
-        DFS(grid, i, j + 1);
-        DFS(grid, i, j - 1);
+    
+        visited[i][j] = true;
+        queue.offer(new int[]{i, j});
+        DFS(grid, i+1, j, queue, visited);
+        DFS(grid, i-1, j, queue, visited);
+        DFS(grid, i, j+1, queue, visited);
+        DFS(grid, i, j-1, queue, visited);
     }
-
-    private int BFS(int[][] grid, int i, int j) {
-        int solution = 0;
-
-        Queue<int[]> bfsQueue = new LinkedList<int[]>();
-        bfsQueue.offer(new int[] {i, j});
-
-        while (!bfsQueue.isEmpty()) {
-            int oldQueueSize = bfsQueue.size();
-
-            for (int k = 0; k < oldQueueSize; ++k) {
-                int[] currentNode = bfsQueue.remove();
-                grid[currentNode[0]][currentNode[1]] = 4;
-                if (currentNode[0] + 1 < grid.length && grid[currentNode[0] + 1][currentNode[1]] == 2 ||
-                    (currentNode[0] - 1 >= 0 && grid[currentNode[0] - 1][currentNode[1]] == 2) ||
-                    (currentNode[1] + 1 < grid[0].length && grid[currentNode[0]][currentNode[1] + 1] == 2) ||
-                    (currentNode[1] - 1 >= 0 && grid[currentNode[0]][currentNode[1] - 1] == 2)) {
-                    
-                    return solution;
-                } else {
-
-                    if (currentNode[0] + 1 <= grid.length - 1 && 
-                        currentNode[1] <= grid.length - 1 && 
-                        currentNode[1] >= 0 && 
-                        currentNode[0] + 1 >= 0 && 
-                        grid[currentNode[0] + 1][currentNode[1]] != 4) 
-                    {
-                        bfsQueue.offer(new int[] {currentNode[0] + 1, currentNode[1]});
-                    }
-                    else if (
-                        currentNode[0] - 1 <= grid.length - 1 && 
-                        currentNode[1] <= grid.length - 1 && 
-                        currentNode[1] >= 0 && currentNode[0] - 1 >= 0 && 
-                        grid[currentNode[0] - 1][currentNode[1]] != 4) 
-                    {
-                        bfsQueue.offer(new int[] {currentNode[0] - 1, currentNode[1]});
-                    }
-                    else if (
-                        currentNode[0] <= grid.length - 1 && 
-                        currentNode[1] + 1 <= grid.length - 1 && 
-                        currentNode[1] + 1 >= 0 && 
-                        currentNode[0] >= 0 && 
-                        grid[currentNode[0]][currentNode[1] + 1] != 4) 
-                    {
-                        bfsQueue.offer(new int[] {currentNode[0], currentNode[1] + 1});
-                    }
-                    else if (
-                        currentNode[0] <= grid.length - 1 && 
-                        currentNode[1] -1 <= grid.length - 1 && 
-                        currentNode[1] -1 >= 0 && 
-                        currentNode[0] >= 0 && 
-                        grid[currentNode[0]][currentNode[1] - 1] != 4) 
-                    {
-                        bfsQueue.offer(new int[] {currentNode[0], currentNode[1] - 1});                        
-                    }
-
-
+    
+    private int BFS(Queue<int[]> queue, boolean[][] visited, int[][] grid){
+        int[][] dirs = {{0,1},{0,-1},{1,0},{-1,0}};
+        int distance = 0;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            while(size-->0){
+                int[] curr = queue.poll();
+                for(int[] dir:dirs){
+                    int x = curr[0] + dir[0];
+                    int y = curr[1] + dir[1];
+                    if(x<0 || y<0 || x>=grid.length || y>=grid[0].length || visited[x][y]) continue;
+    
+                    if(grid[x][y] == 1) return distance;
+    
+                    visited[x][y] = true;
+                    queue.offer(new int[]{x, y});
                 }
             }
-
-            solution++;
+            distance++;
         }
-
-        return solution;
+        return -1;
     }
+    
 }
