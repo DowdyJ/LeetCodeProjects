ac22bb211226911313117c1e6126fc78ae810d9f
DowdyJ <joeldowdy12@gmail.com>
2023-05-24 15:47:54 +0900

    Add working files for 2542

--- 2542 - Maximum Subsequence Score/lc2542/src/main/java/dev/sugaroflead/App.java
+++ 2542 - Maximum Subsequence Score/lc2542/src/main/java/dev/sugaroflead/App.java
@@ -1,19 +1,103 @@
 package dev.sugaroflead;
 
 import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.Queue;
 
 public class App 
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+
+
     }
 
     public long maxScore(int[] nums1, int[] nums2, int k) {
         long maxScore = 0;
 
-        
+        if (nums1.length == k) {
+            long result = 0;
+            for (int num : nums1)
+                result += num;
 
-        return 0;
+            int minMult = Integer.MAX_VALUE;
+            for (int num : nums2) 
+                minMult = num < minMult ? num : minMult;
+
+            result *= minMult;
+            return result;
+        }
+
+
+        quickSort(nums2, nums1, 0, nums1.length - 1);
+
+        int currentInspectionIndex = nums1.length - k;
+
+        while (currentInspectionIndex >= 0) {
+            Queue<Integer> topAdditiveNumbers = new PriorityQueue<>(k);
+            int currentMultiplier = nums2[currentInspectionIndex];
+
+            for (int i = currentInspectionIndex; i < nums1.length; ++i) {
+                topAdditiveNumbers.offer(nums1[i]);
+                if (topAdditiveNumbers.size() > k)
+                    topAdditiveNumbers.poll();
+            }
+
+            long sum = 0;
+            while (topAdditiveNumbers.peek() != null) {
+                sum += topAdditiveNumbers.poll();
+            }
+
+            long product = sum * currentMultiplier;
+            if (product > maxScore)
+                maxScore = product;
+
+            currentInspectionIndex--;
+        }
+
+        return maxScore;
     }
+
+    public void quickSort(int[] arrayToSort, int[] collateralToSort, int low, int high) {
+        if (low < high) {
+            int partitionIndex = partition(arrayToSort, collateralToSort, low, high);
+
+            quickSort(arrayToSort, collateralToSort, low, partitionIndex -1);
+            quickSort(arrayToSort, collateralToSort, partitionIndex + 1, high);
+        }
+    }
+
+    private int partition(int[] arrayToSort, int[] collateralToSort, int low, int high) {
+        int pivot = arrayToSort[high];
+
+        int insertionIndex = low - 1;
+
+        for (int i = low; i < high; ++i) {
+            if (arrayToSort[i] <= pivot) {
+                insertionIndex++;
+
+                int temp = arrayToSort[insertionIndex];
+                arrayToSort[insertionIndex] = arrayToSort[i];
+                arrayToSort[i] = temp;
+
+                temp = collateralToSort[insertionIndex];
+                collateralToSort[insertionIndex] = collateralToSort[i];
+                collateralToSort[i] = temp;
+            }
+        }
+
+        insertionIndex++;
+
+        int temp = arrayToSort[insertionIndex];
+        arrayToSort[insertionIndex] = arrayToSort[high];
+        arrayToSort[high] = temp;
+
+        temp = collateralToSort[insertionIndex];
+        collateralToSort[insertionIndex] = collateralToSort[high];
+        collateralToSort[high] = temp;
+
+        return insertionIndex;
+    }
+
 }
--- 2542 - Maximum Subsequence Score/lc2542/src/test/java/dev/sugaroflead/AppTest.java
+++ 2542 - Maximum Subsequence Score/lc2542/src/test/java/dev/sugaroflead/AppTest.java
@@ -9,6 +9,30 @@ import org.junit.Test;
  */
 public class AppTest 
 {
+
+
+    @Test
+    public void testSort1()
+    {
+        App a = new App();
+        int[] arr1 = new int [] {2,1,14,12};
+        int[] arr2 = new int[] {11,7,13,6};
+        a.quickSort(arr2, arr1, 0, 3);
+        
+        // System.out.println("ARR1");
+        // for (int num : arr1)
+        //     System.out.println(num);
+        
+        // System.out.println("ARR2");
+        // for (int num : arr2)
+        //     System.out.println(num);
+        
+        assertEquals(6, arr2[0]);
+        assertEquals(7, arr2[1]);
+        assertEquals(11, arr2[2]);
+        assertEquals(13, arr2[3]);
+    }    
+
     @Test
     public void testCase1()
     {
@@ -23,5 +47,21 @@ public class AppTest
         App a = new App();
         long max = a.maxScore(new int [] {4,2,3,1,1}, new int[] {7,5,10,9,6}, 1);
         assertEquals(30, max);
+    }
+
+    @Test
+    public void testCase3()
+    {
+        App a = new App();
+        long max = a.maxScore(new int [] {1,4}, new int[] {3,1}, 2);
+        assertEquals(5, max);
+    }
+
+    @Test
+    public void testCase4()
+    {
+        App a = new App();
+        long max = a.maxScore(new int [] {2,1,14,12}, new int[] {11,7,13,6}, 3);
+        assertEquals(168, max);
     }
 }
