2a7d686bb62952dd97b77349b3d1a5905c1e70a6
DowdyJ <joeldowdy12@gmail.com>
2023-05-31 19:07:34 -0700

    Add more optimizations

--- 1091 - Shortest Path in Binary Matrix/lc1091/src/main/java/dev/sugaroflead/lc1091/App.java
+++ 1091 - Shortest Path in Binary Matrix/lc1091/src/main/java/dev/sugaroflead/lc1091/App.java
@@ -18,20 +18,32 @@ public class App
         int ans = a.shortestPathBinaryMatrix(new int[][] {{0,1},{1,0}});
         //assertEquals(2, ans);
     }
+    
 
     public int shortestPathBinaryMatrix(int[][] grid) {
         gridLength = grid.length;
 
+        if (gridLength < 255)
+            return processSmallGrids(grid);
+        else
+            return processNormalGrids(grid);
+
+    }
+
+
+    public int processSmallGrids(int[][] grid) {
         if (gridLength == 0 || grid[0][0] == 1 || grid[gridLength - 1][gridLength - 1] == 1)
             return -1;
 
         int result = 0;
         // Initialized to inital size of just above best case scenario diagonal path
-        Queue<int[]> neighborNodes = new ArrayDeque<int[]>((int)(1.5 * gridLength)) {};
-        neighborNodes.offer(new int[] {0, 0});
+        Queue<char[]> neighborNodes = new ArrayDeque<char[]>((int)(1.5 * gridLength)) {};
+        neighborNodes.offer(new char[] {0, 0});
 
-        int[] coords;
+        char[] coords;
         int oldSize;
+        int newX;
+        int newY;
 
         while(!neighborNodes.isEmpty()) {
             
@@ -42,11 +54,48 @@ public class App
                 
                 if (coords[0] == gridLength - 1 && coords[1] == grid[0].length - 1)
                     return ++result;
-
-                int newX;
-                int newY;
         
-                for (int[] dir : dirs) {
+                for (byte[] dir : dirs) {
+                    newX = coords[0] + dir[0];
+                    newY = coords[1] + dir[1];
+                    if (!(newX < 0 || newY < 0 || newY >= gridLength || newX >= gridLength) && grid[newX][newY] != 1) {
+                        neighborNodes.offer(new char[] {(char)newX, (char)newY});
+                        grid[newX][newY] = 1;
+                    }
+                }
+            }
+
+            result++;
+        }
+
+        return -1;
+    }
+
+    public int processNormalGrids(int[][] grid) {
+        if (gridLength == 0 || grid[0][0] == 1 || grid[gridLength - 1][gridLength - 1] == 1)
+            return -1;
+
+        int result = 0;
+        // Initialized to inital size of just above best case scenario diagonal path
+        Queue<int[]> neighborNodes = new ArrayDeque<int[]>((int)(1.5 * gridLength)) {};
+        neighborNodes.offer(new int[] {0, 0});
+
+        int[] coords;
+        int oldSize;
+        int newX;
+        int newY;
+
+        while(!neighborNodes.isEmpty()) {
+            
+            oldSize = neighborNodes.size();
+            
+            for (int i = 0; i < oldSize; ++i) {
+                coords = neighborNodes.poll();
+                
+                if (coords[0] == gridLength - 1 && coords[1] == grid[0].length - 1)
+                    return ++result;
+        
+                for (byte[] dir : dirs) {
                     newX = coords[0] + dir[0];
                     newY = coords[1] + dir[1];
                     if (!(newX < 0 || newY < 0 || newY >= gridLength || newX >= gridLength) && grid[newX][newY] != 1) {
@@ -64,6 +113,5 @@ public class App
 
     int gridLength = 0;
     
-    static final int[][] dirs = new int[][] {{1,0}, {1,1}, {0,1}, {-1,1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
-
+    static final byte[][] dirs = new byte[][] {{1,0}, {1,1}, {0,1}, {-1,1}, {-1, 0}, {-1, -1}, {0, -1}, {1, -1}};
 }
