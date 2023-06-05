fa9699145c0b5b0ad174b911bfe553f3deee7dee
DowdyJ <joeldowdy12@gmail.com>
2023-06-04 23:27:47 -0700

    Optimize 0050

--- 0050 - Pow(x, n)/solution.c
+++ 0050 - Pow(x, n)/solution.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 double myPow(double x, int n)
 {
@@ -28,13 +29,62 @@ double myPow(double x, int n)
     return res;
 }
 
+
+double myPowFast(double x, int n)
+{
+    if (n == 0 || x == 1)
+        return 1;
+
+    if (x > 1 && n < -2000000000)
+        return 0;
+
+    if (x == -1)
+        return n % 2 == 0 ? x : -x;
+
+    if (x == 0)
+        return 0;
+
+    bool isNegativeExponent = n < 0;
+    if (isNegativeExponent)
+        n *= -1;
+
+    double base = x;
+    double extra = 1;
+
+    if (n > 0) {
+        while (n > 1) {
+            if (n % 2 == 1) {
+                extra *= base;
+                n--;
+                continue;
+            }
+
+            base *= base;
+            n /= 2;
+        } 
+    }
+
+    while (n > 1) {
+        if (n % 2 == 1) {
+            extra *= base;
+            n--;
+            continue;
+        }
+
+        base *= base;
+        n /= 2;
+    }
+
+    return isNegativeExponent ? 1 / (base * extra) : base * extra;
+}
+
 int main()
 {
-    printf("%f\n", myPow(2, 2));
-    printf("%f\n", myPow(2, 1));
-    printf("%f\n", myPow(2, 0));
-    printf("%f\n", myPow(10, 2));
-    printf("%f\n", myPow(5, 2));
+    //printf("%f\n", myPowFast(3, 2));
+    //printf("%f\n", myPowFast(3, 1));
+    //printf("%f\n", myPowFast(3, 0));
+    printf("%f\n", myPowFast(2, 10));
+    //printf("%f\n", myPowFast(5, 4));
 
     return 0;
 }
