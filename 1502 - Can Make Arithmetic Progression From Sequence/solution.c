91d319e08c55e1bb7f16024de38f5337d4ae28b7
DowdyJ <joeldowdy12@gmail.com>
2023-06-05 18:49:38 -0700

    Add optimizations to 1502

--- 1502 - Can Make Arithmetic Progression From Sequence/solution.c
+++ 1502 - Can Make Arithmetic Progression From Sequence/solution.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
 
-int partition(int low, int high, int* arr, int arrSize) {
+int partition(int const low, int const high, int* const arr, int const arrSize) {
 
     int slow = low -1;
     int pivot = arr[high];
@@ -24,7 +26,7 @@ int partition(int low, int high, int* arr, int arrSize) {
     return slow;
 }
 
-void quickSort(int* arr, int arrSize, int low, int high) {
+void quickSort(int* restrict const arr, int const arrSize, int const low, int const high) {
     if (low > high)
         return;
 
@@ -37,7 +39,7 @@ void quickSort(int* arr, int arrSize, int low, int high) {
 
 bool canMakeArithmeticProgression(int* arr, int arrSize){
     quickSort(arr, arrSize, 0, arrSize -1);
-    int targetDifference = arr[1] - arr[0];
+    const int targetDifference = arr[1] - arr[0];
 
     for (int i = 0; i < arrSize - 1; ++i) {
         if (arr[i+1] - arr[i] != targetDifference)
@@ -45,4 +47,17 @@ bool canMakeArithmeticProgression(int* arr, int arrSize){
     }
 
     return true;
+}
+
+int main() {
+    int* arr = malloc(sizeof(int) * 6);
+    arr[0] = 2;
+    arr[1] = 1;
+    arr[2] = 3;
+    arr[3] = 5;
+    arr[4] = 6;
+    arr[5] = 4;
+
+    bool ans = canMakeArithmeticProgression(arr, 6);
+    printf("Ans: %d", ans);
 }
