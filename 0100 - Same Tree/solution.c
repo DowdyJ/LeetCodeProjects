8265b635e6add1ff27093adb8b6d33163235a8cb
DowdyJ <joeldowdy12@gmail.com>
2023-06-08 00:03:57 -0700

    Add solution files for 0100

---
+++ 0100 - Same Tree/solution.c
@@ -0,0 +1,29 @@
+#include <stdbool.h>
+#include <stdlib.h>
+
+struct TreeNode {
+    int val;
+    struct TreeNode *left;
+    struct TreeNode *right;
+};
+
+bool isMatchingDFS(struct TreeNode* restrict left, struct TreeNode* restrict right) {
+    
+    if (left == NULL && right == NULL) {
+        return true;
+    } 
+    else if (left == NULL || right == NULL || left->val != right->val) {
+        return false;
+    }
+
+    return isMatchingDFS(left->left, right->left) && isMatchingDFS(left->right, right->right);
+}
+
+
+bool isSameTree(struct TreeNode* p, struct TreeNode* q) {
+    return isMatchingDFS(p, q);
+}
+
+int main() {
+    return 0;
+}
