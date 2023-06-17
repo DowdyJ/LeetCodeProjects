f57b9127fde94221b118b052b055a4e52fd94b9f
DowdyJ <->
2023-06-17 01:12:42 -0700

    Optimize solution for 1720

--- 1720 - Decode XORed Array/solution.cpp
+++ 1720 - Decode XORed Array/solution.cpp
@@ -10,14 +10,13 @@ const static auto uwu = [] { std::ios::sync_with_stdio(false); std::cin.tie(null
 class Solution {
 public:
     vector<int> decode(vector<int>& encoded, int first) {
-        vector<int> result = vector<int>(encoded.size() + 1, 0);
-        result[0] = first;
+        encoded.insert(encoded.begin(), first);
 
-        for (int i = 1; i < result.size(); ++i) {
-            result[i] = encoded[i-1] ^ result[i-1];
+        for (int i = 1; i < encoded.size(); ++i) {
+            encoded[i] = encoded[i-1] ^ encoded[i];
         }
 
-        return result;
+        return encoded;
     }
 };
 
