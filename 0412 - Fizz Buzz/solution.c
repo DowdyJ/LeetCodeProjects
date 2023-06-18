e6fd65d7972ede1581e857ea5af0d81193bd390b
DowdyJ <->
2023-06-18 00:35:22 -0700

    Add additional solution for 0412

--- 0412 - Fizz Buzz/solution.c
+++ 0412 - Fizz Buzz/solution.c
@@ -3,6 +3,7 @@
 #include <math.h>
 #include <string.h>
 
+// minimal modulo and division
 char ** fizzBuzz(int n, int* returnSize){
 
     const char* fizz = "Fizz"; // 3
@@ -77,6 +78,38 @@ char ** fizzBuzz(int n, int* returnSize){
 
         result[i++ - 1] = fizzbuzz;
         if (i > n) break;                            
+    }
+
+    return result;
+}
+
+// more standard approach
+char ** fizzBuzz2(int n, int* returnSize){
+
+    *returnSize = n;
+    char** result = malloc(sizeof(char*) * n);
+    
+    char* buffer;
+    char buffSize = (char)log10(n) + 2;
+
+    for (int i = 1; i <= n; ++i) {
+       
+        if (i % 5 == 0) {
+            if (i % 3 == 0) {
+                result[i - 1] = "FizzBuzz";
+                continue;
+            }
+
+            result[i - 1] = "Buzz";
+        }
+        else if (i % 3 == 0) {
+            result[i - 1] = "Fizz";
+        }
+        else {
+            buffer = calloc(buffSize, sizeof(char));
+            sprintf(buffer, "%d\0", i);
+            result[i - 1] = buffer;
+        }
     }
 
     return result;
