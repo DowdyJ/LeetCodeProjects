04f8d105db78e082361df0d79ec6a67e2824081b
DowdyJ <joeldowdy12@gmail.com>
2023-06-19 00:11:48 -0700

    Add solution files for 0107

---
+++ 0107 - Binary Tree Level Order Traversal II/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections.Generic;
+
+namespace lc107;
+
+public class TreeNode {
+    public int val;
+    public TreeNode left;
+    public TreeNode right;
+    public TreeNode(int val=0, TreeNode left=null, TreeNode right=null) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
+
+public class Solution {
+    public IList<IList<int>> LevelOrder(TreeNode root) {
+        Queue<TreeNode> nodeQueue = new Queue<TreeNode>();
+
+        nodeQueue.Enqueue(root);
+
+        IList<IList<int>> result = new List<IList<int>>();
+        Stack<IList<int>> backwardResult = new Stack<IList<int>>();
+
+        while (nodeQueue.Count > 0) {
+            IList<int> curResult = new List<int>();
+
+            int count = nodeQueue.Count;
+
+            for (int i = 0; i < count; ++i) {
+                TreeNode cur = nodeQueue.Dequeue();
+                if (cur is null)
+                    continue;
+
+                curResult.Add(cur.val);
+                nodeQueue.Enqueue(cur.left);
+                nodeQueue.Enqueue(cur.right);
+            }
+            
+            if (curResult.Count > 0)
+                backwardResult.Push(curResult);
+        }
+
+        for (int i = 0; i < backwardResult.Count; ++i) {
+            result.Add(backwardResult.Pop());
+        }
+
+        return result;
+    }
+}
