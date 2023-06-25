7ca6f2c7c0e5a02802f31dd92ae829df037e0690
DowdyJ <->
2023-06-25 00:52:27 -0700

    Add solution files for 1204

---
+++ 1204 - Last Person to Fit in the Bus/solution.sql
@@ -0,0 +1,11 @@
+WITH CumulativeWeight AS (
+  SELECT
+    person_name,
+    SUM(weight) OVER(ORDER BY turn) AS cumulative_weight
+  FROM Queue
+)
+SELECT person_name
+FROM CumulativeWeight
+WHERE cumulative_weight <= 1000
+ORDER BY cumulative_weight DESC
+LIMIT 1;
