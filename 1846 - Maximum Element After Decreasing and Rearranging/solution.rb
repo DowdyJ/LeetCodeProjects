62ae7abbda2e004b4dc7d5a45ccbb532d0875d2b
DowdyJ <joeldowdy12@gmail.com>
2023-11-15 11:04:23 +0900

    Revise solution for 1846

--- 1846 - Maximum Element After Decreasing and Rearranging/solution.rb
+++ 1846 - Maximum Element After Decreasing and Rearranging/solution.rb
@@ -1,5 +1,15 @@
 # @param {Integer[]} arr
 # @return {Integer}
+def maximum_element_after_decrementing_and_rearranging_short(arr)
+    arr.sort!.reverse!
+    (0..(arr.length - 2)).each{ |i| arr[i] = arr[i+1] + 1 if arr[i] > arr[i+1] }
+    [arr.max, arr.length].min
+end
+
+def maximum_element_after_decrementing_and_rearranging_comical_oneliner(arr)
+    [arr.sort!.reverse!.each_with_index{ |_, i| arr[i] = arr[i+1] + 1 if arr[i+1] != nil && arr[i] > arr[i+1] }].map{|res| [res.max, res.length].min}[0]
+end
+
 def maximum_element_after_decrementing_and_rearranging(arr)
     potential_res = [arr.max, arr.length].min
     arr.sort!.reverse!
